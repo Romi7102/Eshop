00aee3077101723c08097493b4a9d7e0517770f3
Romi Landau <romi7102@gmail.com>
2023-07-11 14:35:42 +0300

    itay

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import data from './data'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href='/'>EShop</a>
       </header>
+      <main>
+        <h1>Products</h1>
+        {
+          data.products.map(product => (
+            <div key={product.token}>
+              <h2>{product.name}</h2>
+              <img alt='' src={product.img} width={300}></img>
+              <p>{product.description}</p>
+            </div>
+          ))
+        }
+      </main>
     </div>
   );
 }
---
+++ client/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+        {
+            name: "intel core i5-13600kf",
+            category: "Electronics",
+            description: "greate product",
+            price: 100,
+            token: 'peoduct-a-token',
+            brand: 'intel',
+            countInStock: 6,
+            rating: 2.5,
+            numReviews: 100,
+            img: "/imgs/intel.jpg"
+        },
+        {
+            name: "Polo",
+            category: "Pharma",
+            description: "greate product",
+            price: 100,
+            token: 'peoduct-b-token',
+            brand: 'teva',
+            countInStock: 6,
+            rating: 2.5,
+            numReviews: 100,
+            img: "/imgs/polo.jpg"
+        },
+        {
+            name: "Cola zero",
+            category: "Food",
+            description: "greate product",
+            price: 100,
+            token: 'peoduct-c-token',
+            brand: 'teva',
+            countInStock: 6,
+            rating: 2.5,
+            numReviews: 100,
+            img: "/imgs/cola.png"
+        }
+    ]
+}
+
+export default data;
