b4d350d07d9c80574fb6bd976307b854a9a5ce5a
Romi Landau <romi7102@gmail.com>
2023-07-19 10:36:08 +0300

    homePage reducer

--- client/src/pages/HomePage/homePage.js
+++ client/src/pages/HomePage/homePage.js
@@ -1,28 +1,43 @@
 import axios from "axios";
 import Products from "../../components/Products/products";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import MessageBox from "../../components/MessageBox/messageBox";
 import Loading from "../../components/Loading/loading";
+import { HomePageReducer, initState } from "../../Reducers/HomePageReducer";
+import { GET_FAIL, GET_REQUEST, GET_SUCCESS } from "../../Reducers/Actions";
 
 import "./homePage.css";
 
 const HomePage = () => {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(
+    HomePageReducer,
+    initState
+  );
 
   useEffect(() => {
-    axios.get("/products").then((response) => {
-      setProducts(response.data);
-    });
+    const getProducts = async () => {
+      dispatch({ type: GET_REQUEST });
+
+      try {
+        const res = await axios.get("/products");
+        dispatch({ type: GET_SUCCESS, payload: res.data });
+      } catch (e) {
+        dispatch({ type: GET_FAIL, payload: e.message });
+      }
+    };
+    getProducts();
   }, []);
 
   return (
     <>
-    <div>
-        <MessageBox variant="danger">Be carefull</MessageBox>
-        <Loading></Loading>
-    </div>
+      <h1 className="title">Products</h1>
       <div className="products">
-        <h1 className="title">Products</h1>
+        {loading ? (
+          <Loading></Loading>
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : null}
         <Products products={products}></Products>
       </div>
     </>
